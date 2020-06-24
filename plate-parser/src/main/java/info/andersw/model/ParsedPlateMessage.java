a22b964e01d4d5da0709cd478c3fdd7174af38d7
anders <andersw@noone.it>
2020-06-24 14:28:53 +0200

    Cleaner

--- plate-parser/src/main/java/info/andersw/model/ParsedPlateMessage.java
+++ plate-parser/src/main/java/info/andersw/model/ParsedPlateMessage.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.apachecommons.CommonsLog;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 
